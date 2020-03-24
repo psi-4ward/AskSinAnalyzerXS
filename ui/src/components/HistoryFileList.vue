07cf1b10bd006f77f12e3d74d224022069a80997
Christoph Wiechert <wio@psitrax.de>
2020-03-24 01:32:15 +0100

    Show two decimals in csv file size

--- ui/src/components/HistoryFileList.vue
+++ ui/src/components/HistoryFileList.vue
@@ -60,13 +60,12 @@
 <script>
   import { saveAs } from 'file-saver';
 
-  function formatBytes(bytes, decimals = 2) {
+  function formatBytes(bytes) {
     if (bytes === 0) return '0 Bytes';
     const k = 1024;
-    const dm = decimals < 0 ? 0 : decimals;
     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
     const i = Math.floor(Math.log(bytes) / Math.log(k));
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
   }
 
   export default {
