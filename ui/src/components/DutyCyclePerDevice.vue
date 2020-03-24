dba4c51026189c1914872ff1973721ded3533493
Christoph Wiechert <wio@psitrax.de>
2020-03-24 01:10:33 +0100

    Optimize DutyCyclePerDevice Chart

--- ui/src/components/DutyCyclePerDevice.vue
+++ ui/src/components/DutyCyclePerDevice.vue
@@ -27,27 +27,6 @@
     },
 
     computed: {
-      seriesData() {
-        const data = {};
-        const lastTstamp = this.telegram.tstamp - 60 * 60 * 1000;
-        for (let i = this.$service.data.telegrams.length - 1; i >= 0; i--) {
-          const t = this.$service.data.telegrams[i];
-          const to = t.toName || t.toSerial;
-          if (t.fromAddr != this.telegram.fromAddr || t.tstamp > this.telegram.tstamp) continue;
-          if (t.tstamp < lastTstamp) break;
-          if (!data[to]) data[to] = 0;
-          let sendTime = 0;
-          if (t.flags.includes('BURST')) {
-            // 360 ms burst instead of 4 bytes preamble
-            sendTime = 360 + (t.len + 7) * 0.81;
-          } else {
-            sendTime = (t.len + 11) * 0.81;
-          }
-          data[to] += sendTime / 360;
-        }
-        Object.keys(data).forEach(k => data[k] = Math.round(data[k] * 100) / 100);
-        return Object.entries(data).map(([name, y]) => ({ name, y }));
-      },
       dcWarning() {
         const telegrams = this.$service.data.telegrams;
         return this.telegram.tstamp - telegrams[0].tstamp < 60 * 60 * 1000;
@@ -100,7 +79,29 @@
 
     methods: {
       updateData() {
-        this.hightchart.series[0].setData(this.seriesData, true, true);
+        let data = {};
+        const lastTstamp = this.telegram.tstamp - 60 * 60 * 1000;
+        let i;
+        for (i = this.$service.data.telegrams.length - 1; i >= 0; i--) {
+          const t = this.$service.data.telegrams[i];
+          if (t.fromAddr != this.telegram.fromAddr || t.tstamp > this.telegram.tstamp) {
+            continue;
+          } else if (t.tstamp < lastTstamp) break;
+          const to = t.toName || t.toSerial;
+          if (typeof data[to] === "undefined") data[to] = 0;
+          let sendTime = 0;
+          if (t.flags.includes('BURST')) {
+            // 360 ms burst instead of 4 bytes preamble
+            sendTime = 360 + (t.len + 7) * 0.81;
+          } else {
+            sendTime = (t.len + 11) * 0.81;
+          }
+          data[to] += sendTime / 360;
+        }
+        Object.keys(data).forEach(k => data[k] = Math.round(data[k] * 100) / 100);
+        data = Object.entries(data).map(([name, y]) => ({ name, y }));
+
+        this.hightchart.series[0].setData(data, true, true);
       }
     }
   }
