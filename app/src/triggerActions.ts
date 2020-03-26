fe17e34a489be87ca5b488f543af2454e75d4966
Christoph Wiechert <wio@psitrax.de>
2020-03-26 23:31:39 +0100

    Fix: triggerActions does not respect port

--- app/src/triggerActions.ts
+++ app/src/triggerActions.ts
@@ -2,10 +2,10 @@ import {parse} from 'url';
 import {IncomingMessage, request} from "http";
 
 export function exec(action: string, opts: any) {
-  const {protocol, auth, hostname, path} = parse(opts.url);
+  const {protocol, auth, host, port, path} = parse(opts.url);
   return new Promise((resolve, reject) => {
     request({
-        protocol, auth, hostname, path,
+        protocol, auth, host, port, path,
         method: (action === 'httpPost') ? 'POST' : 'GET'
       },
       (res: IncomingMessage) => {
