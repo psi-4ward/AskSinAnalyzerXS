0cbbebc6dd0299cadb1d3dee4c17c5722ac07c74
Christoph Wiechert <wio@psitrax.de>
2020-03-27 20:16:58 +0100

    Fix: triggerActions http request

--- app/src/triggerActions.ts
+++ app/src/triggerActions.ts
@@ -2,10 +2,10 @@ import {parse} from 'url';
 import {IncomingMessage, request} from "http";
 
 export function exec(action: string, opts: any) {
-  const {protocol, auth, host, port, path} = parse(opts.url);
+  const {protocol, auth, hostname, port, path} = parse(opts.url);
   return new Promise((resolve, reject) => {
     request({
-        protocol, auth, host, port, path,
+        protocol, auth, hostname, port, path,
         method: (action === 'httpPost') ? 'POST' : 'GET'
       },
       (res: IncomingMessage) => {
